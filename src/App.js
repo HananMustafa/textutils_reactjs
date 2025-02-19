716d647af25c011fafe21cde66a75eecc615d5aa
Hanan Mustafa <151427566+HananMustafa@users.noreply.github.com>
2025-02-19 11:33:48 -0800

    Changing title dynamically & adding favicons to TextUtils

--- src/App.js
+++ src/App.js
@@ -4,26 +4,27 @@ import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type)=>{
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      type: type
+      type: type,
     });
     setTimeout(() => {
       setAlert(null);
     }, 2000);
-  }
+  };
 
   const toggleMode = () => {
     if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "#042743";
-      showAlert("Dark mode has been enabled", "success")
+      showAlert("Dark mode has been enabled", "success");
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
@@ -40,10 +41,21 @@ function App() {
         mode={mode}
         toggleMode={toggleMode}
       />
-      <Alert alert={alert}/>
+      <Alert alert={alert} />
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert}/>
-        {/* <About/> */}
+          <Routes>
+            <Route
+              exact path="/"
+              element={
+                <TextForm
+                  heading="Enter the text to analyze below"
+                  mode={mode}
+                  showAlert={showAlert}
+                />
+              }
+            />
+            <Route exact path="/about" element={<About />} />
+          </Routes>
       </div>
     </>
   );
